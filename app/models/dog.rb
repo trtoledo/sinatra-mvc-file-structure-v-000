616a67fb504b0293c70a6862251c8bf452ee9863
Tomas Toledo <trtoledo1977@gmail.com>
2018-09-06 02:11:40 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,15 +3,15 @@ class Dog
 
   @@all = []
 
-  def self.all
-    @@all
-  end
-
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
     @@all << self
+  end
+
+  def self.all
+    @@all
   end
 
 end
